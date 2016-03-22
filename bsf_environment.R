931724d5096ea92e93b4bebfac61491c42a20e8f
mkschuster <mschuster@cemm.oeaw.ac.at>
2016-03-22 15:24:12 +0100

    Updated dependencies for the Genome Analysis Toolkit (GATK). Added new Bioconductor package VariantAnnotation required by the bsf_variant_calling_filter.R script.

--- bsf_environment.R
+++ bsf_environment.R
@@ -1,12 +1,13 @@
 #! /usr/bin/env Rscript
 #
-# Rscript to quickly setup the BSF environment after an R upgrade.
+# BSF R script to quickly setup the BSF environment after an R upgrade.
+#
 #
 # Copyright 2013 -2015 Michael K. Schuster
 #
-# Biomedical Sequencing Facility (BSF), part of the genomics core facility
-# of the Research Center for Molecular Medicine (CeMM) of the
-# Austrian Academy of Sciences and the Medical University of Vienna (MUW).
+# Biomedical Sequencing Facility (BSF), part of the genomics core facility of
+# the Research Center for Molecular Medicine (CeMM) of the Austrian Academy of
+# Sciences and the Medical University of Vienna (MUW).
 #
 #
 # This file is part of BSF R.
@@ -26,22 +27,60 @@
 
 source(file = "http://bioconductor.org/biocLite.R")
 
-# CRAN package for BSF R scripts.
-install.packages("optparse")
+# Let the user choose mirror options or ...
+# chooseCRANmirror()
+# chooseBioCmirror()
+# ... set preferences via the options() function in e.g. .Rprofile.
+# options(
+#  repos = c("Austria (Vienna) [https]" = "https://cran.wu.ac.at/"),
+#  BioC_mirror = c("United Kingdom (Hinxton) [https]" = "http://mirrors.ebi.ac.uk/bioconductor")
+# )
+
+
+# Update all CRAN and Bioconductor packages.
+
+update.packages()
+biocUpdatePackages(pkgs = c())
+
+# CRAN packages for BSF R scripts.
+# optparse: option parsing
+# ggplot2: for eveything graphics, really
+#
+# CRAN packages for GATK
+# https://github.com/broadgsa/gatk-protected/blob/master/public/gatk-engine/src/main/resources/org/broadinstitute/gatk/engine/recalibration/BQSR.R
+# gplots:
+# reshape:
+# grid: R system library, no need to install explicitly
+# tools: R system library, no need to install explicitly
+# gsalib:
+
+install.packages(pkgs = c(
+  "optparse",
+  "ggplot2",
+  "gplots",
+  "reshape",
+  "gsalib"
+))
 
 # For the BSF RNA-Seq pipeline
-biocLite(pkgs = c("DESeq", "cummeRbund"))
-
+# cummeRbund
+# DESeq
+#
 # For the BSF ChIP-Seq pipeline
-biocLite(pkgs = c("DiffBind"))
+# DiffBind
+
+biocLite(pkgs = c("DESeq", "cummeRbund", "DiffBind", "VariantAnnotation"))
 
 # For RnBeads
 # The IlluminaHumanMethylation450k.db is > 60 MB
 # TODO: This should be taken care of by an eventual RnBeads package.
-biocLite(pkgs = c("foreach", "mclust", "RPMM", "fields",
-                  "matrixStats", "IlluminaHumanMethylation450k.db", "methylumi",
-                  "ggbio", "GEOquery", "GOstats", "wordcloud"))
+# biocLite(pkgs = c("foreach", "mclust", "RPMM", "fields",
+#                   "matrixStats", "IlluminaHumanMethylation450k.db", "methylumi",
+#                   "ggbio", "GEOquery", "GOstats", "wordcloud"))
 
-# CRAN package for GATK
-install.packages("ggplot2")
-install.packages("gsalib")
+message("All done")
+
+# Finally, print all objects that have not been removed from the environment.
+if (length(x = ls())) {
+  print(x = ls())
+}
