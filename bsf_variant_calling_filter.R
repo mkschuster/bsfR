cb30dd634137a9a721bcb7a270c88908c256b99a
mkschuster <mschuster@cemm.oeaw.ac.at>
2017-04-09 20:26:36 +0200

    Drop the concept of "fields" for data frames in favour of variables (columns) and observations (rows). Added a sessionInfo() call.

--- bsf_variant_calling_filter.R
+++ bsf_variant_calling_filter.R
@@ -3,12 +3,12 @@
 # BSF R script to filter snpEff annotated, multi-sample variant calling format
 # (VCF) files. While SNPEFF_IMPACT values "MODIFIER" and "LOW" are always
 # filtered out, the minor allele frequency (MAF) threshold on the basis of the
-# 1000 Genomes calculated allele frequencies (CAF) field can be configured.
+# 1000 Genomes calculated allele frequencies (CAF) variable can be configured.
 # Individual samples can be selected from the set, as can be a recurrence
 # threshold for each given variant.
 #
 #
-# Copyright 2013 - 2016 Michael K. Schuster
+# Copyright 2013 - 2017 Michael K. Schuster
 #
 # Biomedical Sequencing Facility (BSF), part of the genomics core facility of
 # the Research Center for Molecular Medicine (CeMM) of the Austrian Academy of
@@ -78,7 +78,7 @@ argument_list <- parse_args(object = OptionParser(
     ),
     make_option(
       opt_str = c("--recurrence"),
-      default = 1,
+      default = 1L,
       dest = "recurrence",
       help = "Variant recurrence threshold [1]",
       type = "integer"
@@ -91,7 +91,7 @@ argument_list <- parse_args(object = OptionParser(
     ),
     make_option(
       opt_str = c("--chunk-size"),
-      default = 10000,
+      default = 10000L,
       dest = "chunk_size",
       help = "Chunk size, i.e. number of VCF lines to process at once [10000]",
       type = "integer"
@@ -122,14 +122,14 @@ if (grepl(pattern = "\\W",
 
 # Pre-process the sample names to get a character vector.
 selected_sample_names <- character()
-if (length(x = argument_list$samples) > 0) {
+if (length(x = argument_list$samples) > 0L) {
   selected_sample_names <-
     unlist(x = strsplit(x = argument_list$samples, split = ","))
 }
 
 # Check the recurrence option.
 selected_sample_names_length <- length(x = selected_sample_names)
-if (selected_sample_names_length > 0 &
+if (selected_sample_names_length > 0L &
     selected_sample_names_length < argument_list$recurrence) {
   stop(
     sprintf(
@@ -147,7 +147,7 @@ suppressPackageStartupMessages(expr = library(package = "VariantAnnotation"))
 # Define filter functions for the FilterRules object required by the filterVcf() function.
 
 #' Filter function object for FilterRules to filter out variants with
-#' INFO field SNPEFF_IMPACT equal to "MODIFIER" or "LOW".
+#' INFO variable SNPEFF_IMPACT equal to "MODIFIER" or "LOW".
 #'
 #' @param x: Fully parsed VCF object
 #' @type x: VCF
@@ -175,13 +175,13 @@ filter_info_SNPEFF_IMPACT <- function(x) {
 
 process_CAF_element <- function(x) {
   # If the character vector is empty, retain the record in any case.
-  if (length(x = x) == 0) {
+  if (length(x = x) == 0L) {
     return(TRUE)
   }
   # TODO: Report a BioCoductor VariantAnnotation bug?
-  #   Square brackets in the INFO field are lost, if the last value of a list is missing (i.e. '.').
+  #   Square brackets in the INFO variable are lost, if the last value of a list is missing (i.e. '.').
   # if (length(x = which(x = grepl(pattern = "^\\.$", x = x, perl = TRUE)))) { print(x = x) }
-  # Replace square brackets seemingly specific to the GATK bundle CAF field.
+  # Replace square brackets seemingly specific to the GATK bundle CAF variable.
   x <-
     gsub(
       pattern = "[\\[\\]]",
@@ -214,7 +214,7 @@ filter_info_CAF <- function(x) {
 
 # Filer out variants, which are COMMON.
 #' Filter function object for the FilterRules object to filter out variants with
-#' an INFO field COMMON equal to 1.
+#' an INFO variable COMMON equal to 1.
 #'
 #' @param x: Fully parsed VCF object
 #' @type x: VCF
@@ -223,7 +223,7 @@ filter_info_CAF <- function(x) {
 
 filter_info_COMMON <- function(x) {
   integer_vector <- info(x = x)$db_snp.COMMON
-  logical_vector <- integer_vector != 1
+  logical_vector <- integer_vector != 1L
   # Keep all records with NA.
   logical_vector[is.na(x = logical_vector)] <- TRUE
   return(logical_vector)
@@ -243,9 +243,9 @@ filtered_vcf_path <-
 filtered_tbi_path <-
   paste(filtered_vcf_path, "gz", "tbi", sep = ".")
 
-# Filter the VCF file by the SNPEFF_IMPACT and CAF fields.
+# Filter the VCF file by the SNPEFF_IMPACT and CAF variables.
 if (!(file.exists(filtered_tbi_path) &&
-      (file.info(filtered_tbi_path)$size > 0))) {
+      (file.info(filtered_tbi_path)$size > 0L))) {
   message("Filtering the annotated variants file.")
   filterVcf(
     file = TabixFile(
@@ -291,8 +291,8 @@ selected_tsv_first_chunk <- TRUE
 filtered_vcf_file <- TabixFile(file = filtered_vcf_path,
                                yieldSize = argument_list$chunk_size)
 open(con = filtered_vcf_file)
-sum_records_read <- 0
-sum_records_written <- 0
+sum_records_read <- 0L
+sum_records_written <- 0L
 while (nrow(
   x = vcf_object <- readVcf(
     file = filtered_vcf_file,
@@ -315,30 +315,30 @@ while (nrow(
         x = sub(
           pattern = "_annotated",
           replacement = paste(
-            c("_information_fields", argument_list$set_name),
+            c("_information_variables", argument_list$set_name),
             collapse = "_"
           ),
           x = basename(path = argument_list$vcf_file_path)
         )
       )
     message(paste0(
-      "Writing VCF information fields file: ",
+      "Writing VCF information file: ",
       filtered_vcf_annotation_path
     ))
-    fields_frame <- info(x = vcf_header_object)
-    # Add another "Field" column to properly list row names and reorder the data frame.
-    fields_frame[["Field"]] <- row.names(x = fields_frame)
-    fields_frame <-
-      fields_frame[, c("Field", "Number", "Type", "Description")]
+    info_frame <- info(x = vcf_header_object)
+    # Add another "Variable" column to properly list row names and reorder the data frame.
+    info_frame[["Variable"]] <- row.names(x = info_frame)
+    info_frame <-
+      info_frame[, c("Variable", "Number", "Type", "Description")]
     write.table(
-      x = fields_frame,
+      x = info_frame,
       file = filtered_vcf_annotation_path,
       sep = "\t",
       row.names = FALSE,
       col.names = TRUE
     )
-    rm(filtered_vcf_annotation_path, fields_frame)
-    # Write a DataFrame of Genotype information.
+    rm(filtered_vcf_annotation_path, info_frame)
+    # Write a DataFrame containing VCF header genotype information.
     filtered_vcf_annotation_path <-
       sub(
         pattern = "\\.vcf\\.gz$",
@@ -346,40 +346,42 @@ while (nrow(
         x = sub(
           pattern = "_annotated",
           replacement = paste(c(
-            "_genotype_fields", argument_list$set_name
+            "_genotype_variables", argument_list$set_name
           ), collapse = "_"),
           x = basename(path = argument_list$vcf_file_path)
         )
       )
     message(paste0(
-      "Writing VCF genotype fields file: ",
+      "Writing VCF genotype file: ",
       filtered_vcf_annotation_path
     ))
-    fields_frame <- geno(x = vcf_header_object)
-    # Add another "Field" column to properly list row names and reorder the data frame.
-    fields_frame[["Field"]] <- row.names(x = fields_frame)
-    fields_frame <-
-      fields_frame[, c("Field", "Number", "Type", "Description")]
+    geno_frame <- geno(x = vcf_header_object)
+    # Add another "Variable" column to properly list row names and reorder the data frame.
+    geno_frame[["Variable"]] <- row.names(x = geno_frame)
+    geno_frame <-
+      geno_frame[, c("Variable", "Number", "Type", "Description")]
     write.table(
-      x = fields_frame,
+      x = geno_frame,
       file = filtered_vcf_annotation_path,
       sep = "\t",
       row.names = FALSE,
       col.names = TRUE
     )
-    rm(filtered_vcf_annotation_path, fields_frame)
+    rm(filtered_vcf_annotation_path, geno_frame)
     rm(vcf_header_object)
   }
   
-  # Firstly, retrieve the GRanges object via rowRanges() and convert into a data frame,
-  # whereby several columns need adjusting.
+  # Firstly, retrieve the GRanges object via rowRanges() and convert into a
+  # data frame, whereby several columns need adjusting.
   row_ranges_frame <-
     as.data.frame(x = rowRanges(x = vcf_object, fixed = TRUE))
-  # The original VCF ID field is only available in form of row names.
+  # The original VCF ID variable is only available in form of row names.
   row_ranges_frame$identifier <- row.names(x = row_ranges_frame)
-  # The VCF REF filed is a DNAStringSet object that needs converting into a character vector.
+  # The VCF REF variable is a DNAStringSet object
+  # that needs converting into a character vector.
   row_ranges_frame$REF <- as.character(x = row_ranges_frame$REF)
-  # The ALT column contains a DNAStringSetList object, which individual components need converting into charatcer vectors,
+  # The ALT column contains a DNAStringSetList object,
+  # which individual components need converting into character vectors,
   # in which empty strings need to be replaced with '*' characters (VCF 4.2),
   # before the character vector can be collapsed into a comma-separated string.
   row_ranges_frame$ALT <-
@@ -438,8 +440,8 @@ while (nrow(
         paste(x, collapse = ",")
       }
     ))
-  # FIXME: Because of a bug in VariantAnnotation, the closing square bracket in the CAF field is missing,
-  # if the last value is unknown (i.e. '.'). Bring it back.
+  # FIXME: Because of a bug in VariantAnnotation, the closing square bracket in the
+  # CAF variable is missing, if the last value is unknown (i.e. '.'). Bring it back.
   info_frame$db_snp.CAF <-
     unlist(x = lapply(
       X = info_frame$db_snp.CAF,
@@ -478,28 +480,30 @@ while (nrow(
   column_data_frame <- colData(x = vcf_object)
   # Get sample-specific information as a SimpleList of matrix objects.
   genotype_list <- geno(x = vcf_object)
-  # Create a new data frame for sample-specific information by expanding the column information for each sample name..
+  # Create a new data frame for sample-specific information by expanding the
+  # column information for each sample name..
   sample_frame <- NULL
-  for (sample_name in rownames(x = column_data_frame)) {
-    for (field_name in names(x = genotype_list)) {
-      column_name <- paste(sample_name, field_name, sep = '.')
-      # Exclude the column "SB" (Fisher strand bias), which is an array of dimensions [<variant number>, <sample number>, 4].
-      if (field_name == "SB") {
+  for (sample_name in row.names(x = column_data_frame)) {
+    for (variable_name in names(x = genotype_list)) {
+      column_name <- paste(sample_name, variable_name, sep = '.')
+      # Exclude the column "SB" (Fisher strand bias),
+      # which is an array of dimensions [<variant number>, <sample number>, 4L].
+      if (variable_name == "SB") {
         next()
       }
       if (is.null(x = sample_frame)) {
         # If the sample_frame does not exist, create it.
         sample_frame <-
-          data.frame(column_name = genotype_list[[field_name]][, sample_name])
+          data.frame(column_name = genotype_list[[variable_name]][, sample_name])
         # Change the "column_name" header into its real value.
         names(x = sample_frame) <- column_name
       } else {
         # If the sample frame exists, add a column to it.
         sample_frame[[column_name]] <-
-          genotype_list[[field_name]][, sample_name]
+          genotype_list[[variable_name]][, sample_name]
       }
-      # The AD and PL fields are lists that need collapsing.
-      if (field_name %in% c("AD", "PL")) {
+      # The AD and PL variables are lists that need collapsing.
+      if (variable_name %in% c("AD", "PL")) {
         sample_frame[[column_name]] <- unlist(x = lapply(
           X = sample_frame[[column_name]],
           FUN = function(x) {
@@ -509,7 +513,7 @@ while (nrow(
       }
     }
   }
-  rm(column_name, field_name, sample_name)
+  rm(column_name, variable_name, sample_name)
   
   # Calculate the recurrence of variant sites and store it in an integer vector.
   # Check the "GT" matrix line for line, if a genotype has a number from 1 to 9,
@@ -518,9 +522,9 @@ while (nrow(
   message("Identifying variant sites and calculating their recurrence.")
   info_frame[["Recurrence"]] <-
     integer(length = nrow(x = info_frame))
-  for (i in 1:dim(genotype_list[["GT"]])[1]) {
+  for (i in seq_len(length.out = dim(genotype_list[["GT"]])[1L])) {
     info_frame[i, "Recurrence"] <-
-      sum(grepl(pattern = "[1-9]", x = genotype_list[["GT"]][i,]))
+      sum(grepl(pattern = "[1-9]", x = genotype_list[["GT"]][i, ]))
   }
   rm(i, genotype_list, column_data_frame)
   
@@ -530,9 +534,9 @@ while (nrow(
     cbind(row_ranges_frame, info_frame, sample_frame)
   # Remove the three sub-frames at this stage to save memory.
   rm(row_ranges_frame, info_frame, sample_frame)
-  # Select only rows which Recurrence field is equal to or more than the recurrence threshold option.
+  # Select only rows which Recurrence variable is equal to or more than the recurrence threshold option.
   combined_frame <-
-    combined_frame[combined_frame$Recurrence >= argument_list$recurrence, ]
+    combined_frame[combined_frame$Recurrence >= argument_list$recurrence,]
   
   sum_records_written <-
     sum_records_written + nrow(x = combined_frame)
@@ -551,13 +555,13 @@ while (nrow(
   # For subsequent chunks, the colum names need not writing.
   selected_tsv_first_chunk <- FALSE
 }
-if (sum_records_read > 0) {
+if (sum_records_read > 0L) {
   message(
     sprintf(
       fmt = "Read %i VCF records, wrote %i (%.2f %%).",
       sum_records_read,
       sum_records_written,
-      sum_records_written * 100 / sum_records_read
+      sum_records_written * 100.0 / sum_records_read
     )
   )
 }
@@ -588,3 +592,5 @@ message("All done")
 if (length(x = ls())) {
   print(x = ls())
 }
+
+print(x = sessionInfo())
