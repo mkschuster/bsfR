e5a19d2b41002eb0e69bc305bac94aa325648db1
mkschuster <mschuster@cemm.oeaw.ac.at>
2019-06-04 17:52:27 +0200

    Improved the heat map annotation.
    Automatically retrieve all model terms and annotate them in the ComplexHeatmap.
    Read the contrast summary report that also provides the number of significantly differentially expressed genes.
    Write a HTML report via the Nozzle.R1 CRAN package.

--- bsf_rnaseq_deseq_heatmap.R
+++ bsf_rnaseq_deseq_heatmap.R
@@ -1,6 +1,15 @@
 #!/usr/bin/env Rscript
 #
-# BSF R script to draw a Heatmap from DESeq2 result tables.
+# BSF R script to draw Complex Heatmaps from DESeq2 result tables.
+#
+# For each design, the bsf_rnaseq_deseq_analysis.R script saves DESeqDataSet and
+# DESeqTransform objects to disk, so that running it is a prerequisite. This
+# script then loads the DESeqDataSet object to get access to the column (i.e.
+# sample) annotation (via colData()) and the DESeqTransform object to get access
+# to the transformed counts (via assay()).
+#
+# The contrast summary frame provides the number of significantly differentially
+# expressed genes, per contrast.
 #
 #
 # Copyright 2013 - 2019 Michael K. Schuster
@@ -55,14 +64,14 @@ argument_list <- parse_args(object = OptionParser(
       opt_str = c("--variables"),
       # default = "",
       dest = "variables",
-      help = "Comma-separated list of variables",
+      help = "Comma-separated list of variables [all from design]",
       type = "character"
     ),
     make_option(
       opt_str = c("--maximum-number"),
       default = 400L,
       dest = "maximum_number",
-      help = "Maximum number of genes to plot [400]",
+      help = "Maximum number of genes to plot or -1 for all significant [400]",
       type = "integer"
     ),
     make_option(
@@ -105,11 +114,11 @@ if (is.null(x = argument_list$design_name)) {
 suppressPackageStartupMessages(expr = library(package = "ComplexHeatmap"))
 suppressPackageStartupMessages(expr = library(package = "DESeq2"))
 suppressPackageStartupMessages(expr = library(package = "ggplot2"))
+suppressPackageStartupMessages(expr = library(package = "Nozzle.R1"))
 suppressPackageStartupMessages(expr = library(package = "stringr"))
 
 # Save plots in the following formats.
 
-# FIXME: The graphics_formats are not used for non ggplot2 plots.
 graphics_formats <- c("pdf", "png")
 
 prefix <-
@@ -150,6 +159,8 @@ rm(file_path)
 
 
 # DESeqTransform ----------------------------------------------------------
+
+
 # Load a previously saved "blind" or "model" DESeqTransform object
 # that serves as the base for the Heatmap.
 deseq_transform <- NULL
@@ -177,6 +188,8 @@ rm(file_path)
 
 
 # Contrasts Frame ---------------------------------------------------------
+
+
 # Read a data frame of contrasts with variables "Design", "Numerator", "Denominator" and "Label".
 message("Loading contrast frame")
 contrast_frame <-
@@ -184,7 +197,7 @@ contrast_frame <-
     file = file.path(
       argument_list$genome_directory,
       prefix,
-      paste(prefix, "contrasts.tsv", sep = "_")
+      paste(paste(prefix, "contrasts", "summary", sep = "_"), "tsv", sep = ".")
     ),
     header = TRUE,
     sep = "\t",
@@ -192,24 +205,69 @@ contrast_frame <-
       "Design" = "character",
       "Numerator" = "character",
       "Denominator" = "character",
-      "Label" = "character"
+      "Label" = "character",
+      "Significant" = "integer"
     ),
     stringsAsFactors = FALSE
   )
 # Subset to the selected design.
 contrast_frame <-
-  contrast_frame[contrast_frame$Design == argument_list$design_name, ]
-if (nrow(contrast_frame) == 0L) {
-  stop("No design remaining after selection for design name.")
+  contrast_frame[contrast_frame$Design == argument_list$design_name,]
+if (nrow(x = contrast_frame) == 0L) {
+  stop("No contrast remaining after selection for design name.")
 }
-contrast_frame$Significant <- 0L
 
 # Select column data variables to annotate in the heat map.
+if (is.null(argument_list$variables)) {
+  # Unfortunately, the DESeqDataSet desgin() accessor provides no meaningful
+  # formula, if the design was not full rank. In those cases the formula is set
+  # to ~1 and the Wald testing is based on a model matrix. To get the design
+  # variables, load the initial design data frame and call the all.vars()
+  # function on the formula object.
+  design_frame <- read.table(
+    file = file.path(
+      argument_list$genome_directory,
+      prefix,
+      paste(prefix, "designs.tsv", sep = "_")
+    ),
+    header = TRUE,
+    sep = "\t",
+    colClasses = c(
+      "design" = "character",
+      "full_formula" = "character",
+      "reduced_formulas" = "character",
+      "factor_levels" = "character",
+      "plot_aes" = "character"
+    ),
+    stringsAsFactors = FALSE
+  )
+  design_frame <-
+    design_frame[design_frame$design == argument_list$design_name,]
+  if (nrow(x = design_frame) == 0L) {
+    stop("No design remaining after selection for design name.")
+  }
+  variable_names <-
+    all.vars(expr = as.formula(object = design_frame[1L, "full_formula"]))
+  rm(design_frame)
+} else {
+  variable_names <-
+    stringr::str_split(string = argument_list$variables, pattern = ",")[[1L]]
+}
 column_annotation_frame <-
-  data.frame(colData(x = deseq_data_set)[, stringr::str_split(string = argument_list$variables, pattern = ",")[[1L]], drop = FALSE])
+  data.frame(SummarizedExperiment::colData(x = deseq_data_set)[, variable_names, drop = FALSE])
+rm(variable_names)
+
+# Create a "Contrasts" report section
+nozzle_section_contrasts <-
+  Nozzle.R1::newSection("Contrasts", class = SECTION.CLASS.RESULTS)
+nozzle_section_contrasts <-
+  addTo(parent = nozzle_section_contrasts, Nozzle.R1::newTable(table = contrast_frame))
+nozzle_section_heatmaps <-
+  Nozzle.R1::newSection("Heatmaps", class = SECTION.CLASS.RESULTS)
 
 for (i in seq_len(length.out = nrow(x = contrast_frame))) {
-  # The "contrast" option of the DESeq results() function expects a list of numerator and denominator.
+  # The "contrast" option of the DESeq results() function expects a list of
+  # numerator and denominator.
   contrast_list <-
     list("numerator" = unlist(x = strsplit(x = contrast_frame[i, "Numerator"], split = ",")),
          "denominator" = unlist(x = strsplit(x = contrast_frame[i, "Denominator"], split = ",")))
@@ -222,10 +280,10 @@ for (i in seq_len(length.out = nrow(x = contrast_frame))) {
             "intercept"
           },
           sep = "_")
-  
+
   # Annotated Results Frame -----------------------------------------------
   # Read the annotated data.frame with all genes.
-  
+
   file_path <-
     file.path(argument_list$genome_directory,
               prefix,
@@ -238,11 +296,11 @@ for (i in seq_len(length.out = nrow(x = contrast_frame))) {
                 "tsv",
                 sep = "."
               ))
-  
+
   if (file.exists(file_path) &&
       file.info(file_path)$size > 0L) {
     message(paste0("Loading DESeqResults frame for ", contrast_character))
-    
+
     deseq_results_frame <-
       read.table(
         file = file_path,
@@ -253,27 +311,28 @@ for (i in seq_len(length.out = nrow(x = contrast_frame))) {
     # Reset the row names from the gene_id variable.
     row.names(x = deseq_results_frame) <-
       deseq_results_frame$gene_id
-    
-    # FIXME: Make this configurable so that either top-ranked genes or just the significant genes could be used.
-    # Now try a heat map with the top 400 genes
-    
+
     top_gene_identifiers <-
-      deseq_results_frame[head(
-        x = order(deseq_results_frame$max_rank, decreasing = FALSE),
-        n = argument_list$maximum_number
-      ), "gene_id"]
-    
+      if (argument_list$maximum_number >= 0L) {
+        deseq_results_frame[head(
+          x = order(deseq_results_frame$max_rank, decreasing = FALSE),
+          n = argument_list$maximum_number
+        ), "gene_id"]
+      } else {
+        deseq_results_frame[deseq_results_frame$significant == "yes", "gene_id"]
+      }
+
     # Draw a ComplexHeatmap.
     complex_heatmap <- ComplexHeatmap::Heatmap(
-      # Select the top (gene) rows from the scaled counts matrix and
-      # calculate z-scores per row to center the scale.
-      # Since base::scale() works on columns, two transpositions are required.
-      # The following publication uses log2(TPM + 1) before scaling.
+      # Select the top (gene) rows from the scaled counts matrix and calculate
+      # z-scores per row to center the scale. Since base::scale() works on
+      # columns, two transpositions are required. The following publication uses
+      # log2(TPM + 1) before scaling.
       # http://www.nature.com.ez.srv.meduniwien.ac.at/articles/nature24489/figures/1
       matrix = t(x = base::scale(
         x = t(x = log(x = SummarizedExperiment::assay(
           x = deseq_transform, i = 1L
-        )[top_gene_identifiers, ])),
+        )[top_gene_identifiers,])),
         center = TRUE,
         scale = TRUE
       )),
@@ -291,7 +350,8 @@ for (i in seq_len(length.out = nrow(x = contrast_frame))) {
       column_names_gp = gpar(fontsize = 7),
       top_annotation = ComplexHeatmap::columnAnnotation(df = column_annotation_frame)
     )
-    
+
+    # Add column annotation.
     complex_heatmap <-
       complex_heatmap + ComplexHeatmap::HeatmapAnnotation(
         df = deseq_results_frame[top_gene_identifiers, c("gene_biotype", "significant"), drop = FALSE],
@@ -303,34 +363,84 @@ for (i in seq_len(length.out = nrow(x = contrast_frame))) {
           just = "left"
         )
       )
-    # Add column annotation.
-    
+
+    file_path_character <-
+      paste(
+        paste(
+          prefix,
+          "contrast",
+          contrast_character,
+          "heatmap",
+          suffix,
+          sep = "_"
+        ),
+        graphics_formats,
+        sep = "."
+      )
+
+    # Draw a PDF plot (1L).
     grDevices::pdf(
-      file = file.path(output_directory,
-                       paste(
-                         paste(
-                           prefix,
-                           "contrast",
-                           contrast_character,
-                           "heatmap",
-                           suffix,
-                           sep = "_"
-                         ),
-                         "pdf",
-                         sep = "."
-                       )),
+      file = file.path(output_directory, file_path_character[1L]),
       width = argument_list$plot_width,
       height = argument_list$plot_height
     )
     ComplexHeatmap::draw(object = complex_heatmap)
     base::invisible(x = grDevices::dev.off())
-    rm(complex_heatmap, top_gene_identifiers)
+
+    # Draw a PNG plot (2L).
+    grDevices::png(
+      filename = file.path(output_directory, file_path_character[2L]),
+      width = argument_list$plot_width,
+      height = argument_list$plot_height,
+      units = "in",
+      res = 300L
+    )
+    ComplexHeatmap::draw(object = complex_heatmap)
+    base::invisible(x = grDevices::dev.off())
+
+    nozzle_section_heatmaps <-
+      Nozzle.R1::addTo(
+        parent = nozzle_section_heatmaps,
+        Nozzle.R1::newFigure(
+          file = file_path_character[2L],
+          paste("Heatmap for contrast", contrast_frame[i, "Label"]),
+          fileHighRes = file_path_character[1L]
+        )
+      )
+    rm(complex_heatmap,
+       top_gene_identifiers,
+       file_path_character)
   } else {
     message(paste0("Missing DESeqResults data.frame for ",
                    contrast_character))
   }
-  rm(contrast_character, contrast_list, deseq_results_frame, file_path)
+  rm(contrast_character,
+     contrast_list,
+     deseq_results_frame,
+     file_path)
 }
+
+nozzle_report <-
+  Nozzle.R1::newCustomReport("Complex Heatmap Report", version = 0)
+
+nozzle_report <-
+  Nozzle.R1::addTo(parent = nozzle_report, nozzle_section_contrasts)
+
+nozzle_report <-
+  Nozzle.R1::addTo(parent = nozzle_report, nozzle_section_heatmaps)
+
+Nozzle.R1::writeReport(report = nozzle_report,
+                       filename = file.path(
+                         output_directory,
+                         paste(prefix,
+                               "heatmap",
+                               "report",
+                               suffix,
+                               sep = "_")
+                       ))
+rm(nozzle_report,
+   nozzle_section_heatmaps,
+   nozzle_section_contrasts)
 
 rm(
   i,
